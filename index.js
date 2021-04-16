ee7507a6b44ef87817acda5b56259b33e748ea53
peace920902 <peace9209021@gmail.com>
2021-04-17 02:49:37 +0800

    npm install

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const github = require('@actions/github');
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet', { required: false });
-    console.log(`Hello ${nameToGreet}!`);
+
+    console.log('a' + nameToGreet + 'b');
+    console.log(typeof (nameToGreet));
+    console.log('"": ' + "" === nameToGreet);
+    console.log(null == nameToGreet);
+
+    //console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
